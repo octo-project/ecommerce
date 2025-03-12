12345ef580d75569ae1242f2438c6313dba498f8
TSIALONINA Heriniaina Mathieu <hmathieutsialonina@gmail.com>
2025-03-12 14:44:34 +0300

    fix ui, add cart vue

---
+++ src/components/cart/cart.vue
@@ -0,0 +1,31 @@
+<template>
+    <div v-click-away="" :class="{'invisible': !open, 'absolute flex shadow-md flex-col p-5 justify-between bg-white w-sm right-0 h-screen top-0': true}">
+        <div>
+            <div>
+                <h1 className="text-2xl font-bold mb-4">Shopping Cart</h1>
+            </div>
+            <div className="flex justify-center items-center" :style="{height: 'calc(100vh - 100px)'}">
+                <div v-if="cart.length == 0" className="flex items-center justify-center flex-col">
+                    <img src="@/components/icons/empty.svg" width="80" alt="empty cart" loading="lazy"/>
+                    Your cart is empty.
+                </div>
+            </div>
+        </div>
+        
+        <div>Cart Footer</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import {ref} from 'vue';
+
+    defineProps({
+        open: Boolean
+    })
+
+    const closeCart = () => {
+        console.log("Close cart ...");
+    }
+
+    const cart = ref([]);
+</script>
--- src/components/productCard/ProductCard.vue
+++ src/components/productCard/ProductCard.vue
@@ -1,9 +1,13 @@
 <template>
-    <div className="border rounded-lg shadow-lg p-4 bg-white">
-        <img :src="product.image" :alt="product.title" className="w-full h-48 object-scale-down rounded"/>
-        <h2 className="text-lg font-semibold mt-2">{{ product.title }}</h2>
-        <p className="text-gray-600 mt-1">{{ product.price }} $</p>
-        <button @click="goToDetails(product.id)" className="mt-3 bg-blue-500 cursor-pointer text-white px-4 py-2 rounded hover:bg-blue-700">View Details</button>
+    <div className="rounded-lg shadow-lg p-4 bg-white">
+        <img :src="product.image" :alt="product.title" className="w-full h-48 object-scale-down rounded" loading="lazy"/>
+        <div className="flex flex-col justify-between">
+            <div className="flex flex-col flex-grow justify-around min-h-[108px]">
+                <h2 className="text-lg font-semibold !mt-2 !text-gray-800 line-clamp-1">{{ product.title }}</h2>
+                <p className="text-gray-600 mt-1">{{ product.price }} $</p>
+            </div>
+            <button @click="goToDetails(product.id)" className="mt-3 bg-blue-500 cursor-pointer text-white px-4 py-2 rounded hover:bg-blue-700">View Details</button>
+        </div>
     </div>
 </template>
 
