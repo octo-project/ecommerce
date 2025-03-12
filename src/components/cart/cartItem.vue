5a7d609a683760d1611fe7a54007918e5098bbbd
TSIALONINA Heriniaina Mathieu <hmathieutsialonina@gmail.com>
2025-03-12 23:59:56 +0300

    add cart item

---
+++ src/components/cart/cartItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <div className="flex justify-between items-center">
+        <span>Product : {{ props.productId }}</span>
+        <span>Quantity : {{ props.quantity }}</span>
+        <img @click="removeProduct(props.productId)" className="cursor-pointer" src="@/components/icons/trash.svg" width="40" alt="trash" loading="lazy">
+    </div>
+</template>
+
+<script setup lang="ts">
+    const props = defineProps<{
+        quantity: number;
+        productId: number; 
+    }>()
+
+    const removeProduct = (productId: number) => {
+        console.log("remove product from cart : ", productId);
+    }
+</script>
