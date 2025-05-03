ccd60e1e833560ad5d4064e9f96c180f118cfad4
TSIALONINA Heriniaina Mathieu <hmathieutsialonina@gmail.com>
2025-05-03 10:27:26 +0300

    fix product details responsive design

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="w-full min-h-screen relative">
+    <div class="w-full min-h-screen">
         <NavBar/>
         <div v-if="loading" className="text-center">Loading ...</div>
         <div v-else-if="error" className="text-red-500">{{ error }}</div>
-        <div v-else className="product-detail-container flex p-5 items-center" :style="{ height: 'calc(100vh - 80px)' }">
+        <div v-else className="product-detail-container flex flex-col md:flex-row p-5 items-center" :style="{ height: 'calc(100vh - 80px)' }">
             <img :src="product.image" :alt="product.name" className="h-64 object-scale-down rounded" loading="lazy"/>
-            <div className="!ml-5">
+            <div className="!ml-5 pb-[15px] md:pb-0">
                 <h1 className="text-2xl font-bold mt-4">{{ product.name }}</h1>
-                <p className="product-description text-gray-600 !mt-2">{{ product.description }}</p>
+                <p className="product-description text-gray-600 !mt-2 text-justify">{{ product.description }}</p>
                 <p className="text-xl font-semibold !my-4">
                     Price : 
                     <span className="!font-bold">{{ product.price }} $</span>
